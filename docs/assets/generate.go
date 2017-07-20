2c6de303a9cedceb2df59b7ec16bf35341def673
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-07-19 23:19:42 -0500

    delete deprecated storage docs

--- docs/assets/generate.go
+++ docs/assets/generate.go
@@ -6,8 +6,4 @@ package assets
 //go:generate dot -Tpng -o nbd_semideduped_storage.png src/nbd_semideduped_storage.dot
 //go:generate dot -Tpng -o nbd_tlog_storage.png src/nbd_tlog_storage.dot
 
-//go:generate dot -Tpng -o deduped_get.png src/deduped_get.dot
-
-//go:generate dot -Tpng -o tlog_storage.png src/tlog_storage.dot
-
 //go:generate dot -Tpng -o zerodisk_overview.png src/zerodisk_overview.dot
