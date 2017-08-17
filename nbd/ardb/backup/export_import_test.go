6706289d51cb9435ce89f651c10f13fdcb28d094
decauwsemaecker.glen@gmail.com <decauwsemaecker.glen@gmail.com>
2017-08-17 16:09:17 -0500

    add more import_export communative tests

--- nbd/ardb/backup/export_import_test.go
+++ nbd/ardb/backup/export_import_test.go
@@ -8,38 +8,73 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/zero-os/0-Disk/log"
+	"github.com/zero-os/0-Disk/nbd/ardb"
 	"github.com/zero-os/0-Disk/nbd/ardb/storage"
+	"github.com/zero-os/0-Disk/redisstub"
 )
 
-func TestImportExportCycle_8_2_16(t *testing.T) {
-	testImportExportCycle(t, 8, 2, 16)
+func TestImportExportCommute_8_2_16_MS(t *testing.T) {
+	testImportExportCommute(t, 8, 2, 16, newInMemoryStorage)
 }
 
-func TestImportExportCycle_16_8_32(t *testing.T) {
-	testImportExportCycle(t, 16, 8, 32)
+func TestImportExportCommute_16_8_32_MS(t *testing.T) {
+	testImportExportCommute(t, 16, 8, 32, newInMemoryStorage)
 }
 
-func TestImportExportCycle_64_8_32(t *testing.T) {
-	testImportExportCycle(t, 64, 8, 32)
+func TestImportExportCommute_64_8_32_MS(t *testing.T) {
+	testImportExportCommute(t, 64, 8, 32, newInMemoryStorage)
 }
 
-func TestImportExportCycle_8_8_32(t *testing.T) {
-	testImportExportCycle(t, 8, 8, 32)
+func TestImportExportCommute_64_8_32_DS(t *testing.T) {
+	testImportExportCommute(t, 64, 8, 32, newDedupedStorage)
 }
 
-func TestImportExportCycle_64_64_32(t *testing.T) {
-	testImportExportCycle(t, 64, 64, 32)
+func TestImportExportCommute_8_8_32_MS(t *testing.T) {
+	testImportExportCommute(t, 8, 8, 32, newInMemoryStorage)
 }
 
-func TestImportExportCycle_8_16_32(t *testing.T) {
-	testImportExportCycle(t, 8, 16, 32)
+func TestImportExportCommute_64_64_32_MS(t *testing.T) {
+	testImportExportCommute(t, 64, 64, 32, newInMemoryStorage)
 }
 
-func TestImportExportCycle_8_64_32(t *testing.T) {
-	testImportExportCycle(t, 8, 64, 32)
+func TestImportExportCommute_64_64_32_DS(t *testing.T) {
+	testImportExportCommute(t, 64, 64, 32, newDedupedStorage)
 }
 
-func testImportExportCycle(t *testing.T, srcBS, dstBS, blockCount int64) {
+func TestImportExportCommute_8_16_32_MS(t *testing.T) {
+	testImportExportCommute(t, 8, 16, 32, newInMemoryStorage)
+}
+
+func TestImportExportCommute_8_64_32_MS(t *testing.T) {
+	testImportExportCommute(t, 8, 64, 32, newInMemoryStorage)
+}
+
+func TestImportExportCommute_8_64_32_DS(t *testing.T) {
+	testImportExportCommute(t, 8, 64, 32, newDedupedStorage)
+}
+
+type storageGenerator func(t *testing.T, vdiskID string, blockSize int64) (storage.BlockStorage, func())
+
+func newInMemoryStorage(t *testing.T, vdiskID string, blockSize int64) (storage.BlockStorage, func()) {
+	storage := storage.NewInMemoryStorage(vdiskID, blockSize)
+	return storage, func() {
+		storage.Close()
+	}
+}
+
+func newDedupedStorage(t *testing.T, vdiskID string, blockSize int64) (storage.BlockStorage, func()) {
+	redisProvider := redisstub.NewInMemoryRedisProvider(nil)
+	storage, err := storage.Deduped(vdiskID, blockSize, ardb.DefaultLBACacheLimit, false, redisProvider)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return storage, func() {
+		redisProvider.Close()
+		storage.Close()
+	}
+}
+
+func testImportExportCommute(t *testing.T, srcBS, dstBS, blockCount int64, sgen storageGenerator) {
 	assert := assert.New(t)
 
 	ibm, indices := generateImportExportData(srcBS, blockCount)
@@ -55,7 +90,8 @@ func testImportExportCycle(t *testing.T, srcBS, dstBS, blockCount int64) {
 	defer cancel()
 
 	// setup source in-memory storage
-	srcMS := storage.NewInMemoryStorage(vdiskID, srcBS)
+	srcMS, srcMSClose := sgen(t, vdiskID, srcBS)
+	defer srcMSClose()
 	// store all blocks in the source
 	for index, block := range ibm {
 		err = srcMS.SetBlock(index, block)
@@ -82,7 +118,8 @@ func testImportExportCycle(t *testing.T, srcBS, dstBS, blockCount int64) {
 	}
 
 	// setup destination in-memory storage
-	dstMS := storage.NewInMemoryStorage(vdiskID, srcBS)
+	dstMS, dstMSClose := sgen(t, vdiskID, srcBS)
+	defer dstMSClose()
 
 	// import into destination in-memory storage
 	importCfg := importConfig{
@@ -98,22 +135,17 @@ func testImportExportCycle(t *testing.T, srcBS, dstBS, blockCount int64) {
 		return
 	}
 
-	var srcBlock, dstBlock []byte
+	var dstBlock []byte
 
 	// ensure that both source and destination contain
 	// the same blocks for the same indices
-	for _, index := range indices {
-		srcBlock, err = srcMS.GetBlock(index)
-		if !assert.NoError(err) {
-			continue
-		}
-
+	for index, block := range ibm {
 		dstBlock, err = dstMS.GetBlock(index)
 		if !assert.NoError(err) {
 			continue
 		}
 
-		assert.Equal(srcBlock, dstBlock)
+		assert.Equal(block, dstBlock)
 	}
 }
 
