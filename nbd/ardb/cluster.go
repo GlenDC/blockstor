b65364b078346945fceca5a1db1435f337cd5c69
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2017-09-30 16:58:34 -0500

    go into the newer (simpler) direction

--- nbd/ardb/cluster.go
+++ nbd/ardb/cluster.go
@@ -2,10 +2,20 @@ package ardb
 
 import (
 	"errors"
-	"sync"
 
 	"github.com/zero-os/0-Disk/config"
 )
+
+// HotReloading of a cluster shouldn't be part of this package,
+// as it isn't generic enough, and it wouldn't be as efficient as it could be!
+
+// StorageCluster TODO: finish
+type StorageCluster interface {
+	ServerConfig() (*ServerConfig, error)
+	ServerConfigAt(serverIndex int64) (*ServerConfig, error)
+	ServerConfigFor(objectIndex int64) (*ServerConfig, error)
+	ServerIndexFor(objectIndex int64) (int64, error)
+}
 
 // TODO:
 // how to set server states and react on those changes?
@@ -13,6 +23,13 @@ import (
 // TODO:
 // should we simply make the dynamic reloading a standalone spawned goroutine
 // which manages a cluster?!
+
+// ClusterPair TODO
+type ClusterPair struct {
+	first, second *Cluster
+}
+
+// TODO: define ClusterPair methods
 
 // NewCluster creates a new ARDB Cluster.
 func NewCluster(servers ...config.StorageServerConfig) *Cluster {
@@ -28,73 +45,46 @@ func NewCluster(servers ...config.StorageServerConfig) *Cluster {
 type Cluster struct {
 	servers     []config.StorageServerConfig
 	serverCount int64
-	serverType  ServerType
 
-	slaveServers     []config.StorageServerConfig
-	slaveServerCount int64
-	slaveServerType  ServerType
-
-	mux sync.RWMutex
+	// TODO:
+	// + add cluster type
+	// + make goroutine safe
 }
 
-// SetPrimaryServers allows you to set primary servers for the given cluster.
-// You need to give at least as much servers as the slave servers defined,
-// an error will be returned oterhwise.
-func (cluster *Cluster) SetPrimaryServers(servers ...config.StorageServerConfig) error {
-	count := int64(len(servers))
-
-	cluster.mux.Lock()
-	defer cluster.mux.Unlock()
-
-	if count == 0 || count < cluster.slaveServerCount {
-		return ErrInsufficientServers
-	}
-	cluster.servers = servers
-	cluster.serverCount = count
-
-	return nil
+// SetServers TODO
+func (cluster *Cluster) SetServers(servers ...config.StorageServerConfig) error {
+	return errors.New("TODO")
 }
 
-// SetSlaveServers allows you to (un)set slave servers for the given cluster.
-// You need to give at least as much servers as the primary servers defined,
-// an error will be returned oterhwise
-func (cluster *Cluster) SetSlaveServers(servers ...config.StorageServerConfig) error {
-	cluster.mux.Lock()
-	defer cluster.mux.Unlock()
-
-	count := int64(len(servers))
-	if servers != nil && count < cluster.serverCount {
-		return ErrInsufficientServers
-	}
-
-	cluster.slaveServers = servers
-	cluster.slaveServerCount = count
-	return nil
+// SetServerState TODO
+func (cluster *Cluster) SetServerState(serverIndex int64, state config.StorageServerState) error {
+	return errors.New("TODO")
 }
 
-// ServerConfig returns the ServerConfig for a given object index.
-func (cluster *Cluster) ServerConfig(index int64) (*ServerConfig, error) {
-	cluster.mux.RLock()
-	defer cluster.mux.RUnlock()
-
-	// get the index of an operational primary or slave server
-	serverIndex, err := cluster.serverIndex(index)
-	if err != nil {
-		return nil, err
+// ServerConfig returns the first available server.
+func (cluster *Cluster) ServerConfig() (*ServerConfig, error) {
+	for serverIndex, server := range cluster.servers {
+		if server.State == config.StorageServerStateOnline {
+			return &ServerConfig{
+				Address:  server.Address,
+				Database: server.Database,
+				Index:    int64(serverIndex),
+			}, nil
+		}
 	}
 
-	// if we get a valid index,
-	// we know for sure that it is either for a primary or slave server,
-	// which is operational.
-	//
-	// TODO: this serverType could be any of the following combinations:
-	//   1. primary | slave
-	//   2. slave   | primary
-	//   3. primary | N/A
-	// ... now however it is assumed to be always situation (1)
-	server, serverType := cluster.servers[index], ServerTypePrimary
+	return nil, ErrNoServersAvailable
+}
+
+// ServerConfigAt returns the ServerConfig at a given server index.
+func (cluster *Cluster) ServerConfigAt(serverIndex int64) (*ServerConfig, error) {
+	if serverIndex < 0 || serverIndex >= cluster.serverCount {
+		return nil, ErrServerIndexOOB
+	}
+
+	server := cluster.servers[serverIndex]
 	if server.State != config.StorageServerStateOnline {
-		server, serverType = cluster.slaveServers[index], ServerTypeSlave
+		return nil, ErrServerNotOnline
 	}
 
 	// return the configuration for the operational server
@@ -102,25 +92,34 @@ func (cluster *Cluster) ServerConfig(index int64) (*ServerConfig, error) {
 		Address:  server.Address,
 		Database: server.Database,
 		Index:    serverIndex,
-		Type:     serverType,
 	}, nil
 }
 
-// ServerIndex returns the (modulo) index of a server,
-// based on a given index of an object.
-func (cluster *Cluster) ServerIndex(index int64) (int64, error) {
-	cluster.mux.RLock()
-	defer cluster.mux.RUnlock()
-	return cluster.serverIndex(index)
+// ServerConfigFor returns the ServerConfig for a given object index.
+func (cluster *Cluster) ServerConfigFor(objectIndex int64) (*ServerConfig, error) {
+	// get the server index of an operational server of this cluster
+	serverIndex, err := cluster.ServerIndexFor(objectIndex)
+	if err != nil {
+		return nil, err
+	}
+	server := cluster.servers[objectIndex]
+
+	// return the configuration for the operational server
+	return &ServerConfig{
+		Address:  server.Address,
+		Database: server.Database,
+		Index:    serverIndex,
+		//Type:     serverType, // TODO
+	}, nil
 }
 
-// serverIndex returns the (modulo) index of a server,
-// based on a given index of an object.
-func (cluster *Cluster) serverIndex(index int64) (int64, error) {
+// ServerIndexFor returns the server index of a server,
+// for a given index of an object.
+func (cluster *Cluster) ServerIndexFor(objectIndex int64) (int64, error) {
 	// first try the modulo sharding,
 	// which will work for all default online shards
 	// and thus keep it as cheap as possible
-	serverIndex := index % cluster.serverCount
+	serverIndex := objectIndex % cluster.serverCount
 	if cluster.servers[serverIndex].State == config.StorageServerStateOnline {
 		return serverIndex, nil
 	}
@@ -137,7 +136,7 @@ func (cluster *Cluster) serverIndex(index int64) (int64, error) {
 	var j int64
 	var key uint64
 	for {
-		key = uint64(index)
+		key = uint64(objectIndex)
 		j = 0
 		for j < cluster.serverCount {
 			serverIndex = j
@@ -148,7 +147,7 @@ func (cluster *Cluster) serverIndex(index int64) (int64, error) {
 			return serverIndex, nil
 		}
 
-		index++
+		objectIndex++
 	}
 }
 
@@ -166,15 +165,7 @@ func (cluster *Cluster) operational() bool {
 // serverOperational returns true if
 // the primary or slave server for the given index is online.
 func (cluster *Cluster) serverOperational(index int64) bool {
-	if cluster.servers[index].State == config.StorageServerStateOnline {
-		return true
-	}
-
-	if cluster.slaveServers != nil && cluster.slaveServers[index].State == config.StorageServerStateOnline {
-		return true
-	}
-
-	return false
+	return cluster.servers[index].State == config.StorageServerStateOnline
 }
 
 // ServerConfig defines the config for an ARDB server.
@@ -183,30 +174,6 @@ type ServerConfig struct {
 	Database int
 	Index    int64
 	Type     ServerType
-}
-
-// ClusterType defines a type of ardb cluster.
-type ClusterType uint8
-
-// The different cluster types available.
-const (
-	ClusterTypePrimarySlave ClusterType = iota
-	ClusterTypeSlavePrimary
-	ClusterTypeTemplate
-)
-
-// String returns the ServerType as a string.
-func (ct ClusterType) String() string {
-	switch ct {
-	case ClusterTypePrimarySlave:
-		return "primary-to-slave"
-	case ClusterTypeSlavePrimary:
-		return "slave-to-primary"
-	case ClusterTypeTemplate:
-		return "template"
-	default:
-		return ""
-	}
 }
 
 // ServerType defines a type of ardb server.
@@ -234,6 +201,13 @@ const (
 )
 
 var (
+	// ErrServerNotOnline is returned in case
+	// a server is requested which isn't online according to its state.
+	ErrServerNotOnline = errors.New("server is not online")
+	// ErrServerIndexOOB is returned in case
+	// a given server index used is out of bounds,
+	// for the cluster context it is used within.
+	ErrServerIndexOOB = errors.New("server index out of bounds")
 	// ErrNoServersAvailable is returned in case
 	// no servers are available for usage.
 	ErrNoServersAvailable = errors.New("no servers available")
